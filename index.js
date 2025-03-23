5902d4b05452cb49b8d4187bf94db56fae3d192f
Jasdeep Singh <j.singhhh917@gmail.com>
2025-03-23 16:15:52 +1100

    making it an executable

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 
 // the reason we dont require 'process.cwd' is because the process module is one of the only modules automatically added to the global scope of every project.
