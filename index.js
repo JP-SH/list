b0355c5719b7dc099e06d2d616903146a82f1ab2
Jasdeep Singh <j.singhhh917@gmail.com>
2025-03-23 17:40:56 +1100

    identifying if it's a file or folder: bad and optional solution

--- index.js
+++ index.js
@@ -8,5 +8,39 @@ fs.readdir(process.cwd(), (err, filenames) => {
     console.log(err);
   }
 
-  console.log(filenames);
+  // how to get items in the highlighted to show what they are such as a file or folder
+
+  //  A BAD WAY TO WRITE IT WOULD BE
+  // for (let filename of filenames) {
+  //   fs.lstat(filename, (err, stats) => {
+  //     if (err) {
+  //       console.log(err);
+  //     }
+
+  //     console.log(filename, stats.isFile());
+  //   });
+  // }
+
+  // optional solution
+  const allStats = Array(filenames.length).fill(null);
+    for (let filename of filenames) {
+      const index = filenames.indexOf(filename);
+    fs.lstat(filename, (err, stats) => {
+      if (err) {
+        console.log(err);
+      }
+
+      allStats[index] = stats;
+
+      const ready = allStats.every((stats) => {
+        return stats;
+      });
+
+      if (ready) {
+        allStats.forEach((stats, index) => {
+          console.log(filenames[index], stats.isFile());
+        });
+      }
+    });
+  }
 });
