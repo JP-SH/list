26ad09a61a3ab7b5870b59e98674a8e8f007c8eb
Jasdeep Singh <j.singhhh917@gmail.com>
2025-03-23 16:02:42 +1100

    process cwd

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+// the reason we dont require 'process.cwd' is because the process module is one of the only modules automatically added to the global scope of every project.
+fs.readdir(process.cwd(), (err, filenames) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log(filenames);
+});
